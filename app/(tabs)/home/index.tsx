c475d2674ebc919829f54e98ddffcc6e4f760afe
Samuel <samuel.profissional18@gmail.com>
2026-02-13 20:25:20 -0300

    feat: add Home and Navbar components with styles

---
+++ app/(tabs)/home/index.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import { styles } from "./Home.styles";
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      
+    </View>
+  );
+}
